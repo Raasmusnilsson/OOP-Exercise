99b6292fef0906d0507d13b24586a56a53825d4b
Raasmus nilsson <raasmusnilsson@gmail.com>
2021-12-19 19:59:23 +0100

    Refactoring

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ConsoleApp4
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
@@ -30,73 +29,6 @@ namespace ConsoleApp4
             foreach (Animal animal in myshelter)
             {
                 animal.MakeSound();
-            }
-        }
-        public abstract class Animal
-        {
-            public string Name { get; set; }
-            public int Id { get; set; }
-            public Animal(string name, int id)
-            {
-                Name = name;
-                Id = id;
-            }
-            internal string GetName()
-            {
-                return Name;
-            }
-            internal void MakeSound()
-            {
-                Console.WriteLine("Voff!"); 
-            }
-        }
-        public class Dog : Animal
-        {
-            public Dog(string name, int id) : base(name, id) { }
-        }
-        public class Shelter
-        {
-            public List<Animal> Dogs = new();
-            public Shelter() { }
-            public void AddAnimal(Animal animal)
-            {
-                Dogs.Add(animal);
-            }
-            public void DeleteAnimal(string name)
-            {
-                foreach (Animal animal in Dogs)
-                {
-                    if (animal.Name == name)
-                    {
-                        Dogs.Remove(animal);
-                        break;
-                    }
-                }
-            }
-            public void DeleteAnimal(int id)
-            {
-                foreach (Animal animal in Dogs)
-                {
-                    if (animal.Id == id)
-                    {
-                        Dogs.Remove(animal);
-                        break;
-                    }
-                }
-            }
-            public void UpdateAnimal(Animal dog, string newName)
-            {
-                dog.Name = newName;
-            }
-            public Animal GetAnimal(int id)
-            {   
-               var dog = Dogs.Find(dog => dog.Id == id);
-               
-                return dog;
-            }
-            public List<Animal> GetAnimals()
-            {
-                return Dogs;
             }
         }
     }
